47a32a86ae366570341f46ab052c684dd0711420
tomabechi yuri <yuri.0123h@icloud.com>
2025-10-13 11:01:58 +0900

    修正

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+            'email' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->numerify('080#######'),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->secondaryAddress(),
+            'category_id' => $this->faker->numberBetween(1, 5), 
+            'detail' => $this->faker->realText(100),
+        ];
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CategoriesTableSeeder::class);
+        \App\Models\Contact::factory(35)->create();
     }
 }
